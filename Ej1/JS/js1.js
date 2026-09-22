ae0721bab69280f661d7d25779130c0df3b6c99d
JavierGarRod <fgarrod408@g.educaand.es>
2026-09-22 12:32:01 +0200

    Subo actividad reaso

---
+++ Ej1/JS/js1.js
@@ -0,0 +1,7 @@
+var nombre = prompt("Introduce tu nombre: ");
+var sueldo = prompt("Introduce tu sueldo: ");
+var sueldoNeto= sueldo*0.85;
+
+alert("Hola "+nombre+", tu sueldo neto es: "+sueldoNeto)
+
+document.write("<h1>Hola " + nombre + "</h1>");
