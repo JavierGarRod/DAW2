e56d6cab11973499a45fc1670bee04c1c959b312
JavierGarRod <fgarrod408@g.educaand.es>
2026-09-24 11:00:52 +0200

    Subo actividad reaso

---
+++ Practica1/Ej2/JS/js1.js
@@ -0,0 +1,4 @@
+var cadena = prompt("Introduce una cadena de texto: ");
+var num=prompt("Introduce un número: ");
+
+alert( "En la posición " + num + " de la cadena\n" + cadena + " se encuentra el carácter " + cadena[num] + "'" );
